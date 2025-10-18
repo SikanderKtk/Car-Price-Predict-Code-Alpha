989b1dec3b2a3ba2a673fe469f315e041fc920ca
Sikander Ktk <sikanderktk222@gmail.com>
2025-10-18 17:01:40 +0500

    Change header color and adjust content in app.py
    Updated the 'About This App' section styling and content.

--- app.py
+++ app.py
@@ -120,14 +120,13 @@ with col_img:
 
 with col_about:
     st.markdown("""
-    <h2 style='text-align:center; color:#FFD700; margin-bottom:10px;'>About This App</h2>
+    <h2 style='text-align:center; color:Blue; margin-bottom:10px;'>About This App</h2>
     <div style='text-align:center; font-size:1rem; color:black; line-height:1.7; background:rgba(255,255,255,0.05); padding:20px; border-radius:15px; box-shadow:0 4px 12px rgba(0,0,0,0.3); max-width:650px; margin:auto;'>
         <p>
             This interactive web app predicts <b style='color:#FFD700;'>car prices</b> using a trained 
             <b style='color:#FFD700;'>Machine Learning model</b>. <br><br>
             Enter your car details in the sidebar to get an accurate market price estimate instantly.  
-            The model considers important features such as <b>brand, fuel type, transmission, car age,</b> 
-            and <b>driven kilometers</b> to generate reliable results. <br><br>
+            <br>
             This project demonstrates a full <b style='color:#FFD700;'>end-to-end ML workflow</b> — 
             from <b>data preprocessing</b> to 
             <b>real-time prediction deployment</b> using Streamlit.
